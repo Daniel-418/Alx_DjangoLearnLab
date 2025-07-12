ae743cc5490619388b81cfdafb178e7b4ad6734d
root <root@Vector.localdomain>
2025-07-11 23:25:18 -0600

    .

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # customize admin columns
+    list_display = ('title', 'author', 'publication_year')  # show these columns
+    list_filter = ('author', 'publication_year')             # enable sidebar filters
+    search_fields = ('title', 'author')                      # enable search bar
 
 admin.site.register(Book, BookAdmin)
