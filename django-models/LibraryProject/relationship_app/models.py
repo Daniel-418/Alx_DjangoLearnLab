9fca868bcb85dc5a2fe8c97b5d3128ac9073ccac
root <root@Vector.localdomain>
2025-07-17 19:51:05 -0600

    2

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,21 +8,21 @@ class Author(models.Model):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=(models.CASCADE))
 
     def __str__(self):
-        return self.name
+        return self.title
 
 class Library(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.PROTECT)
 
     def __str__(self):
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,11 @@
 from django.db import models
-from .models import Author, Librarian, Book
+from .models import Author, Librarian, Book, Library
 
-author = Author(name="Daniel")
-author.save()
-author.
+author = Author.objects.get(name="Author_name")
+author.book_set.all()
+
+lib = Library.objects.get(name="Library_name")
+books = lib.books.all()
+
+lib = Library.objects.get(name="Library_name")
+librarian = lib.librarian
