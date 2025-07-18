608a430f003adb8fd67937a11e5404ba29506a70
root <root@Vector.localdomain>
2025-07-17 20:05:45 -0600

    .

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,8 @@ from django.db import models
 from .models import Author, Librarian, Book, Library
 library_name = "Prince Albert Library"
 
-author = Author.objects.get(name=Author_name)
+author = Author.objects.get(name=author_name)
+books = Book.objects.filter(author=author)
 author.book_set.all()
 
 lib = Library.objects.get(name=library_name)
