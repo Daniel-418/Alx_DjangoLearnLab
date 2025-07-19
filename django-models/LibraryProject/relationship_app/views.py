a4ea3b57cd0d721e480756361839bdce6b549fe6
root <root@Vector.localdomain>
2025-07-18 23:55:41 -0600

    .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from .models import Book
+
 
 # Create your views here.
+def booklist(request):
+    books = {"books" : Book.objects.all()}
+
+    return render(request, "list_books.html", books)
