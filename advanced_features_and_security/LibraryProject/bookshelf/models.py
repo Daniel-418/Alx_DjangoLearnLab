7f7441b61b7ee09db53163702a5abd39c839eaa2
root <root@Vector.localdomain>
2025-08-03 18:19:48 -0600

    m

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -13,9 +13,25 @@ class CustomUser(AbstractUser):
     profile_photo = models.ImageField()
 
 #User manager
-class MyUserManager(BaseUserManager):
-    def create_user(self, username, date_of_birth, profile_photo):
-        user = self.model(username=username, date_of_birth=date_of_birth,
-                          profile_photo=profile_photo)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password, 
+                    date_of_birth, profile_photo, **extra_fields):
+        user = self.model(username=username, password=password,
+                          date_of_birth=date_of_birth,
+                          profile_photo=profile_photo, **extra_fields)
         user.save()
         return user
+
+    def create_superuser(self, username, 
+                         password, date_of_birth, 
+                         profile_photo, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(
+            username=username,
+            password=password,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+            **extra_fields)
