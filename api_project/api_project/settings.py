060ba300d98c4b0c0462e0cee82bd9efe8727563
root <root@Vector.localdomain>
2025-08-09 20:39:49 -0600

    .

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -130,7 +130,6 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
