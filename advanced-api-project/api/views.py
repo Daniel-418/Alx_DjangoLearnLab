9d366c3b9f79a1e3bb60e9f93639bb4be450bf55
root <root@Vector.localdomain>
2025-08-16 18:41:13 -0600

    .

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,26 +5,40 @@ from .models import Book
 
 # Create your views here.
 class ListView(generics.ListAPIView):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self): #pyright: ignore
+        return Book.objects.filter(owner=self.request.user)
 
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    def get_queryset(self): #pyright: ignore
+        return Book.objects.filter(owner=self.request.user)
+
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self): #pyright: ignore
+        return Book.objects.filter(owner=self.request.user)
 
 class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self): #pyright: ignore
+        return Book.objects.filter(owner=self.request.user)
+
 class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self): #pyright: ignore
+        return Book.objects.filter(owner=self.request.user)
