ecc25f68f3743e77317e69b88927be3bffcf19c6
root <root@Vector.localdomain>
2025-08-16 18:23:07 -0600

    .

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.views.generic import DetailView
+from .views import (ListView, UpdateView,
+                    DeleteView, CreateView,
+                    DetailView)
+
+urlpatterns = [
+    path("books/", ListView.as_view(), name="list"),
+    path("books/<int:pk>/", DetailView.as_view(), name="detail"),
+    path("books/<int:pk>/update/", UpdateView.as_view(), name="update"),
+    path("books/create/", CreateView.as_view(), name="create",),
+    path("books/<int:pk>/delete/", DeleteView.as_view(), name="delete")
+]
